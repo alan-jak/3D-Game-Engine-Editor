1585e0cea69ff2a28f1ef5f6cb9b76926df119d4
dragonCASTjosh <dragonCASTjosh@gmail.com>
2014-04-08 09:51:57 +0100

    Cleaned project a little (code mostly same)
    please add comments to your code

---
+++ src/com/editor/core/Main.java
@@ -0,0 +1,17 @@
+package com.editor.core;
+
+public class Main
+{
+    public static boolean editor = false;
+
+    public static void Main(String[] args)
+    {
+
+        // runs init method of StringPhaser
+        StringPhaser.init();
+
+        while (editor)
+            StringPhaser.update();
+    }
+
+}
